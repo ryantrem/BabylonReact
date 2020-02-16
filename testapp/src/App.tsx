19e8adadd348b3cfc337fcdf2925fe3ca843b067
Ryan Tremblay <ryan.tremblay@outlook.com>
2020-02-16 11:30:05 -0800

    Remove default reactjs view from template
    Make EngineViews a larger fixed size

--- testapp/src/App.tsx
+++ testapp/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import * as Babylon from 'babylonjs';
 import './App.css';
 import * as BabylonReact from './BabylonReact'
@@ -49,22 +48,8 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <BabylonReact.EngineView camera={camera1} />
-        <BabylonReact.EngineView camera={camera2} />
-      </header>
+      <BabylonReact.EngineView width={500} height={500} camera={camera1} />
+      <BabylonReact.EngineView width={500} height={500} camera={camera2} />
     </div>
   );
 }
